4c67e64df8a0315f74ea8068ce6be2207a8a98eb
william hill <williamhill0831@Hotmail.com>
2023-09-01 23:36:21 -0400

    drop down menu

--- script.js
+++ script.js
@@ -10,3 +10,19 @@ function toggleModal() {
 
 openButton.addEventListener('click', toggleModal);
 closeButton.addEventListener('click', toggleModal);
+
+const dropdownContainer = document.querySelector('.dropdown-container');
+const menuTitle = document.querySelector('.menu-title');
+const dropdownMenu = document.querySelector('.dropdown-menu');
+
+menuTitle.addEventListener('click', (e) => {
+  if (e.target === e.currentTarget) {
+    dropdownMenu.classList.toggle('visible');
+  }
+});
+
+window.addEventListener('click', (e) => {
+  if (!dropdownContainer.contains(e.target)) {
+    dropdownMenu.classList.remove('visible');
+  }
+});
